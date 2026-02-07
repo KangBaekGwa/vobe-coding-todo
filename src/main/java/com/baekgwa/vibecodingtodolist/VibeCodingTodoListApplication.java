d81db2b0c913022daae6f76de4c1b54b67c0811e
KangBaekGwa <ksu9801@gmail.com>
2026-02-07 19:03:43 +0900

    feat: Spring Boot 프로젝트 초기화

---
+++ src/main/java/com/baekgwa/vibecodingtodolist/VibeCodingTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.baekgwa.vibecodingtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeCodingTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeCodingTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baekgwa/vibecodingtodolist/VibeCodingTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baekgwa.vibecodingtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeCodingTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
